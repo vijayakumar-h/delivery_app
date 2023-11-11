bb897a4aa2451fd63e2fcf74d5bbe9bf2cde1771
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-11 20:18:04 +0530

    added the categories dummy data in app controller.

--- lib/features/controller/app_controller.dart
+++ lib/features/controller/app_controller.dart
@@ -15,5 +15,35 @@ mixin _CategoryControllerMixin {
       "Vegetables",
       "(20)",
     ),
+    Category(
+      2,
+      AppIcons.fruits,
+      "Fruits",
+      "(21)",
+    ),
+    Category(
+      3,
+      AppIcons.bread,
+      "Bread",
+      "(22)",
+    ),
+    Category(
+      4,
+      AppIcons.sweets,
+      "Sweets",
+      "(23)",
+    ),
+    Category(
+      5,
+      AppIcons.pasta,
+      "Pasta",
+      "(24)",
+    ),
+    Category(
+      6,
+      AppIcons.drinks,
+      "Drinks",
+      "(25)",
+    ),
   };
 }
