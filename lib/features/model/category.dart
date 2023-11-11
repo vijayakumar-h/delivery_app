c1aa8bf59389c1d5e62c9b69c74e6a46ff86e6a5
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-11 19:38:21 +0530

    created the category model.

---
+++ lib/features/model/category.dart
@@ -0,0 +1,13 @@
+class Category {
+  final int id;
+  final String image;
+  final String category;
+  final String productCode;
+
+  Category(
+    this.id,
+    this.image,
+    this.category,
+    this.productCode,
+  );
+}
