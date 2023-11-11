6b0f4de83b3c98faa519d2b3e1719bcaa08dd290
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-11 19:40:43 +0530

    created the category card file.

---
+++ lib/widgets/category_card_widget.dart
@@ -0,0 +1,67 @@
+import 'package:delivery_app/utils/common_exports.dart';
+
+class CategoryCardWidget extends StatelessWidget {
+  final Category category;
+  const CategoryCardWidget({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Theme.of(context).highlightColor,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(8),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 140,
+            width: 180,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColorDark.withOpacity(0.3),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                ),
+              ),
+              child: Image.asset(
+                category.image,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  category.category,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  category.productCode,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
