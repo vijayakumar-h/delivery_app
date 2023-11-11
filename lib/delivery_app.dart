1c2b9fad662db55745f1ba355d26d321d3650271
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-11 19:42:11 +0530

    updated the common urls and added the app controller instance in initial file.

--- lib/delivery_app.dart
+++ lib/delivery_app.dart
@@ -1,4 +1,7 @@
+import 'package:delivery_app/features/controller/app_controller.dart';
 import 'package:delivery_app/utils/common_exports.dart';
+
+final appController = AppController();
 
 class DeliveryApp extends StatefulWidget {
   const DeliveryApp({super.key});
@@ -27,18 +30,82 @@ class _DeliveryAppState extends State<DeliveryApp> {
       appBar: AppBar(
         automaticallyImplyLeading: true,
       ),
-      body: const Column(
+      body: Column(
         children: [
-          Text(
+          const Text(
             "Categories",
             style: TextStyle(
               fontSize: 34,
               fontWeight: FontWeight.w700,
             ),
           ),
-          SearchTextBarWidget(
+          const SearchTextBarWidget(
             placeHolder: 'Search',
             icon: Icons.search,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ...appController.getCategory.map(
+            (e) => DecoratedBox(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).highlightColor,
+                ),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(8),
+                ),
+                // color: Theme.of(context).primaryColorDark.withOpacity(0.3),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 140,
+                    width: 180,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color:
+                            Theme.of(context).primaryColorDark.withOpacity(0.3),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(8),
+                          topRight: Radius.circular(8),
+                        ),
+                      ),
+                      child: Image.asset(
+                        e.image,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          e.category,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          e.productCode,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: Theme.of(context).primaryColorDark,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ],
       ),
