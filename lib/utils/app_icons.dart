4af5d4bce5129c77214f7b4a32b7344a61fd4571
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-11 20:13:08 +0530

    added the image url in app icon file.

--- lib/utils/app_icons.dart
+++ lib/utils/app_icons.dart
@@ -1,4 +1,9 @@
 class AppIcons {
   static const String dIcon = "assets/images/d-50.png";
+  static const String pasta = "assets/images/pasta.png";
+  static const String bread = "assets/images/bread.png";
+  static const String drinks = "assets/images/drinks.png";
+  static const String sweets = "assets/images/sweets.png";
+  static const String fruits = "assets/images/fruites.png";
   static const String vegetables = "assets/images/vegetables.png";
 }
