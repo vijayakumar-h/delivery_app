74838155bcbd8fb835eddc169dda08cd257f6306
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-13 21:27:00 +0530

    updated the app bottom navigation file.

--- lib/app_bottom_navigation_bar.dart
+++ lib/app_bottom_navigation_bar.dart
@@ -9,27 +9,27 @@ class AppBottomNavigationBar extends StatelessWidget {
       valueListenable: appController.currentIndex,
       builder: (_, index, __) => BottomNavigationBar(
         currentIndex: index,
-          selectedItemColor: Theme.of(context).shadowColor,
-          unselectedItemColor: Colors.grey,
-          onTap: (value)=> appController.currentIndex.value = value,
-          items: [
-            BottomNavigationBarItem(
-                label: "Home",
-                icon: const Icon(Icons.apps_outlined),
-                activeIcon: Icon(Icons.apps_outlined,
-                    color: Theme.of(context).shadowColor)),
-            BottomNavigationBarItem(
-                label: "Payment",
-                icon: const Icon(Icons.shopping_cart_rounded),
-                activeIcon: Icon(Icons.shopping_cart_rounded,
-                    color: Theme.of(context).shadowColor)),
-            BottomNavigationBarItem(
-                label: "Profile",
-                icon: const Icon(Icons.person_2_rounded),
-                activeIcon: Icon(Icons.person_2_outlined,
-                    color: Theme.of(context).shadowColor)),
-          ],
-        ),
+        selectedItemColor: Theme.of(context).shadowColor,
+        unselectedItemColor: Colors.grey,
+        onTap: (value) => appController.currentIndex.value = value,
+        items: [
+          BottomNavigationBarItem(
+              label: "Home",
+              icon: const Icon(Icons.apps_outlined),
+              activeIcon: Icon(Icons.apps_outlined,
+                  color: Theme.of(context).shadowColor)),
+          BottomNavigationBarItem(
+              label: "Payment",
+              icon: const Icon(Icons.shopping_cart_rounded),
+              activeIcon: Icon(Icons.shopping_cart_rounded,
+                  color: Theme.of(context).shadowColor)),
+          BottomNavigationBarItem(
+              label: "Profile",
+              icon: const Icon(Icons.person_2_rounded),
+              activeIcon: Icon(Icons.person_2_outlined,
+                  color: Theme.of(context).shadowColor)),
+        ],
+      ),
     );
   }
 }
