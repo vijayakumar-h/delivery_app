48f10f5c12d5ca8b76a5fb455346541276178eb7
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-13 21:46:47 +0530

    created the check out title widget file.

---
+++ lib/features/cart_payment/widget/checkout_title_widget.dart
@@ -0,0 +1,40 @@
+import 'package:delivery_app/utils/common_exports.dart';
+
+class CheckoutTitleWidget extends StatelessWidget {
+  final String title;
+  final String buttonTitle;
+  final void Function()? onTap;
+
+  const CheckoutTitleWidget({
+    super.key,
+    required this.title,
+    required this.buttonTitle,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextButton(
+            onPressed: onTap,
+            child: Text(
+              buttonTitle,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Theme.of(context).shadowColor,
+              ),
+            ))
+      ],
+    );
+  }
+}
