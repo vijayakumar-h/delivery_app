25c14caa945ba2f058c69ec0a9b20e226b56fff9
VijayHonnegowda <vijay.kumar@repalogic.com>
2023-11-13 21:29:12 +0530

    updated the page indicator.

--- lib/widgets/page_indicator_widget.dart
+++ lib/widgets/page_indicator_widget.dart
@@ -12,23 +12,23 @@ class PageIndicatorWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ...List.generate(
-          length,
-          (index) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              height: currentPage == index ? 10 : 8,
-              width: currentPage == index ? 10 : 8,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: currentPage == index ? Colors.white : Colors.grey,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ...List.generate(
+            length,
+            (index) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                height: currentPage == index ? 10 : 8,
+                width: currentPage == index ? 10 : 8,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: currentPage == index ? Colors.white : Colors.grey,
+                ),
               ),
             ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
 }
